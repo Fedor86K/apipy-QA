880d82151f9fac830f0cddc594e5e9ecc962cd43
Fedor <120029783+Fedor86K@users.noreply.github.com>
2023-03-23 04:40:46 +0500

    upd

---
+++ QApipy.py
@@ -0,0 +1,17 @@
+
+print("\n" + " "*10 + "Welcome to QApiPy launcher!" + " "*10)
+
+choice = input("\nPress key 1 (POST mode) or key 2 (GET mode)...")
+
+if choice == "1":
+    exec(open('post.py').read())
+    
+if choice == "2":
+    exec(open('get.py').read())
+    
+else:
+    pass
+
+input("\nPress ""ENTER"" to exit...")
+
+
