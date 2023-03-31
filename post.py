896afabcb3465c288fecb257462773d530ed1656
Fedor <120029783+Fedor86K@users.noreply.github.com>
2023-04-01 03:31:17 +0500

    Update post.py

--- post.py
+++ post.py
@@ -40,7 +40,7 @@ def run(test,url,send,check,number):
         data = response.json()
         string = json.dumps(data,indent = 4)
 
-        if str(status) == check:
+        if status == check:
             result = "PASS"
         else:
             result = "FAIL"
